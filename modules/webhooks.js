e59cde8bd27718d288e485bee83f2d8ad6a25935
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2022-10-03 01:11:35 -0500

    home_directory is needed in multiple places, oops

--- modules/webhooks.js
+++ modules/webhooks.js
@@ -4,11 +4,11 @@ const router = express.Router();
 // We need to execute some bash scripts on webhook execution
 const execSync = require('child_process').execSync;
 
+const home_directory = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
 var self_hook = "do-not-use"
 
 if (require('os').hostname() == "illinifurs.com") {
     // Keep the actual webhooks we use secret
-    const home_directory = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
     const secrets = require(home_directory + '/secrets/secret.json');
     self_hook = secrets["git-path-self"];
 }
