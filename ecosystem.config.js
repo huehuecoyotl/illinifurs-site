447ccde15b7d3019140c04fdda50ffc915309f0d
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2022-10-20 19:39:18 -0500

    updating ecosystem.config.js to new repo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: ['illinifurs.com'],
       key: '~/.ssh/IlliniFurs-AWS.pem',
       ref: 'origin/main',
-      repo: 'git@illinifurs.com:illinifurs-site',
+      repo: 'git@github.com:huehuecoyotl/illinifurs-site',
       path: '/apps/site',
       'post-deploy': './post_deploy.sh'
     }
